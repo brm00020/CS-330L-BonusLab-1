373a24ac4842885f960aea5cb228be2d080cb68e
brm00020 <brm00020@mix.wvu.edu>
2025-05-01 15:20:01 -0400

    committing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import CustomCard from './Card';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 330L</h1>
       <h2>Section - 011</h2>
       <p>WVU ID: 800407787</p>
       <p>Hi I am Benjamin Marsh</p>
+      <CustomCard />
     </div>
   );
 }
